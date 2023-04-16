339a4bd6fe335936df194b64c16988b71a8aefb0
KidalaZ <buhryakov15@mail.ru>
2023-04-16 23:37:15 +0300

    lr2 and lr3

--- OS/LR1/archivator.cpp
+++ OS/LR1/archivator.cpp
@@ -6,65 +6,77 @@
 
 using namespace std;
 
-class Zipper{
+class Zipper
+{
 
 private:
-    vector<string> files;   // набор файлов (-files)
-    string path;            // путь (-path)    
-    string real_bin_file;   // имя выходного файла-архива( используется при архивации )
+    vector<string> files; // набор файлов (-files)
+    string path;          // путь (-path)
+    string real_bin_file; // имя выходного файла-архива( используется при архивации )
 public:
     Zipper(vector<string> &vec, string p)
     {
-          if(vec.size()>0) files.assign(vec.begin(),vec.end());
-          path = p+"/";
-          real_bin_file=path+"binary.zipper";
+        if (vec.size() > 0)
+            files.assign(vec.begin(), vec.end());
+        path = p + "/";
+        real_bin_file = path + "binary.zipper";
     }
 
-    void getInfo();   // Метод для получения информации о файлах на этапе архивации
-    void InCompress();   // Архивация данных
-    void OutCompress(string binary);   // Распаковка данных ( binary - путь до архива )
+    void getInfo();                  // Метод для получения информации о файлах на этапе архивации
+    void InCompress();               // Архивация данных
+    void OutCompress(string binary); // Распаковка данных ( binary - путь до архива )
 
-    // Статический метод для выделения имени файла из полного пути. 
+    // Статический метод для выделения имени файла из полного пути.
     // Используется для внутренних нужд.
-    static string get_file_name(string fn){return fn.substr(fn.find_last_of("/")+1,fn.size());}
+    static string get_file_name(string fn) { return fn.substr(fn.find_last_of("/") + 1, fn.size()); }
 };
 
 int digs(double w)
 {
     int yield = 0;
-    while(w>10) {yield++;w/=10;}
-    return yield+1;
-} 
+    while (w > 10)
+    {
+        yield++;
+        w /= 10;
+    }
+    return yield + 1;
+}
 
 void Zipper::InCompress()
 {
-    char byte[1];  // единичный буфер для считывания одного байта
+    char byte[1]; // единичный буфер для считывания одного байта
 
-    getInfo();  // получаем необходимую информацию о том, что архивируем
+    getInfo(); // получаем необходимую информацию о том, что архивируем
 
     FILE *f;
-    FILE *main=fopen((this->real_bin_file).c_str(),"wb");  // файл - архив
-    FILE *info = fopen((this->path+"info.txt").c_str(),"rb");  // файл с информацией
+    FILE *main = fopen((this->real_bin_file).c_str(), "wb");     // файл - архив
+    FILE *info = fopen((this->path + "info.txt").c_str(), "rb"); // файл с информацией
 
     // переписываем информацию в архив
-    while(!feof(info))
+    while (!feof(info))
     {
-        if(fread(byte,1,1,info)==1) fwrite(byte,1,1,main);
-        }
+        if (fread(byte, 1, 1, info) == 1)
+            fwrite(byte, 1, 1, main);
+    }
 
-        fclose(info);
-        remove((this->path+"info.txt").c_str());  // прибираемся за собой
+    fclose(info);
+    remove((this->path + "info.txt").c_str()); // прибираемся за собой
 
     // последовательная запись в архив архивируемых файлов побайтно :
-    for(vector<string>::iterator itr=this->files.begin();itr!=this->files.end();++itr)
+    for (vector<string>::iterator itr = this->files.begin(); itr != this->files.end(); ++itr)
     {
-        f = fopen((*itr).c_str(),"rb");
-        if(!f){ cout<<*itr<<" не найден!"<<endl; break;}
-        while(!feof(f))
+        f = fopen((*itr).c_str(), "rb");
+        if (!f)
         {
-            if(fread(byte,1,1,f)==1) fwrite(byte,1,1,main);
+            cout << *itr << " не найден!" << endl;
+            break;
         }
-        cout<<*itr<<" добавлен в архив '"<<this->real_bin_file<<"'."<<endl;
+        while (!feof(f))
+        {
+            if (fread(byte, 1, 1, f) == 1)
+                fwrite(byte, 1, 1, main);
+        }
+        cout << *itr << " добавлен в архив '" << this->real_bin_file << endl;
         fclose(f);
     }
     fclose(main);
@@ -72,29 +84,30 @@ void Zipper::InCompress()
 
 void Zipper::getInfo()
 {
-    char byte[1];  // единичный буфер для считывания одного байта
+    char byte[1]; // единичный буфер для считывания одного байта
 
     basic_string<char> s_info = "";
-    remove((this->path+"info.txt").c_str());  // на всякий случай
-    FILE *info = fopen((this->path+"info.txt").c_str(),"a+");  // сохраняем информацию в наш текстовый файл
-    int bytes_size=0;  // длина информационного блока в байтах
-    for(vector<string>::iterator itr=this->files.begin();itr!=this->files.end();++itr)
+    remove((this->path + "info.txt").c_str());                   // на всякий случай
+    FILE *info = fopen((this->path + "info.txt").c_str(), "a+"); // сохраняем информацию в наш текстовый файл
+    int bytes_size = 0;                                          // длина информационного блока в байтах
+    for (vector<string>::iterator itr = this->files.begin(); itr != this->files.end(); ++itr)
     {
-        FILE *f = fopen((*itr).c_str(),"rb");
-        if(!f) break;
+        FILE *f = fopen((*itr).c_str(), "rb");
+        if (!f)
+            break;
 
-         // получаем размер архивируемого файла
-        fseek(f,0,SEEK_END);
-        int size = ftell(f);             
+        // получаем размер архивируемого файла
+        fseek(f, 0, SEEK_END);
+        int size = ftell(f);
 
-        string name = Zipper::get_file_name(*itr);  // получаем имя архивируемого файла
+        string name = Zipper::get_file_name(*itr); // получаем имя архивируемого файла
 
         char *m_size = new char[digs(size)];
-        sprintf(m_size,"%d", size);
+        sprintf(m_size, "%d", size);
         fclose(f);
 
-        bytes_size+=digs(size);
-        bytes_size+=strlen(name.c_str());
+        bytes_size += digs(size);
+        bytes_size += strlen(name.c_str());
 
         // все, что "нарыли", сохраняем в промежуточный буфер :
         s_info.append(m_size);
@@ -102,75 +115,77 @@ void Zipper::getInfo()
         s_info.append(name);
         s_info.append("||");
 
-        delete [] m_size;
-
+        delete[] m_size;
     }
-    bytes_size = s_info.size()+2;
+    bytes_size = s_info.size() + 2;
     char *b_buff = new char[digs(bytes_size)];
     sprintf(b_buff, "%d", bytes_size);
 
     // форматируем до 5 байт
-    if(digs(bytes_size)<5) fputs(string(5-digs(bytes_size),'0').c_str(),info);
+    if (digs(bytes_size) < 5)
+        fputs(string(5 - digs(bytes_size), '0').c_str(), info);
 
-    fputs(b_buff,info);
-    fputs("||",info);
-    fputs(s_info.c_str(),info);
+    fputs(b_buff, info);
+    fputs("||", info);
+    fputs(s_info.c_str(), info);
 
     fclose(info);
 }
 
 void Zipper::OutCompress(string binary)
 {
-    FILE *bin = fopen(binary.c_str(),"rb");   // открываем архив в режиме чтения
-    char info_block_size[5];   // размер информационного блока 
-    fread(info_block_size,1,5,bin);  // получаем размер
-    int _sz = atoi(info_block_size);  // преобразуем буфер в число
+    FILE *bin = fopen(binary.c_str(), "rb"); // открываем архив в режиме чтения
+    char info_block_size[5];                 // размер информационного блока
+    fread(info_block_size, 1, 5, bin);       // получаем размер
+    int _sz = atoi(info_block_size);         // преобразуем буфер в число
 
-    char *info_block = new char[_sz];  // информационный блок
-    fread(info_block,1,_sz,bin);   // считываем его 
+    char *info_block = new char[_sz]; // информационный блок
+    fread(info_block, 1, _sz, bin);   // считываем его
 
     // Парсинг информационного блока :
     vector<string> tokens;
-    char *tok = strtok(info_block,"||");
+    char *tok = strtok(info_block, "||");
     int toks = 0;
-    while(tok)
+    while (tok)
     {
-        if(strlen(tok)==0) break;
+        if (strlen(tok) == 0)
+            break;
         tokens.push_back(tok);
-        tok=strtok(NULL,"||");
+        tok = strtok(NULL, "||");
         toks++;
     }
 
-    if(toks%2==1) toks--;  // удаляем мусор
-    int files=toks/2;  // количество обнаруженных файлов в архиве
+    if (toks % 2 == 1)
+        toks--;           // удаляем мусор
+    int files = toks / 2; // количество обнаруженных файлов в архиве
 
-    char byte[1];   // единичный буфер для считывания одного байта
+    char byte[1]; // единичный буфер для считывания одного байта
 
     // Процесс распаковки всех файлов( по правилам полученным из блока с информацией ) :
-    for(int i=0;i<files;i++)
+    for (int i = 0; i < files; i++)
     {
-        const char* size = tokens[i*2].c_str();
-        const char* name = tokens[i*2+1].c_str();
+        const char *size = tokens[i * 2].c_str();
+        const char *name = tokens[i * 2 + 1].c_str();
         char full_path[255];
-        strcpy(full_path,this->path.c_str());
-        strcat(full_path,name);
+        strcpy(full_path, this->path.c_str());
+        strcat(full_path, name);
         int _sz = atoi(size);
-        cout<<"--  '"<<name<<"' извлечен в '"<<this->path<<"' ."<<endl;
-        FILE *curr = fopen(full_path,"wb");
-        for(int r=1;r<=_sz;r++)
+        cout << "--  '" << name << "' извлечен в '" << this->path << endl;
+        FILE *curr = fopen(full_path, "wb");
+        for (int r = 1; r <= _sz; r++)
         {
-            if(fread(byte,1,1,bin)==1) fwrite(byte,1,1,curr);
+            if (fread(byte, 1, 1, bin) == 1)
+                fwrite(byte, 1, 1, curr);
         }
         fclose(curr);
 
-        delete [] size;
-        delete [] name;
+        delete[] size;
+        delete[] name;
     }
     fclose(bin);
-
 }
 
-int main(int argv, char* argc[])
+int main(int argv, char *argc[])
 {
     /*/  Supported args:
     //
@@ -178,32 +193,59 @@ int main(int argv, char* argc[])
     //
     /*/
 
-    setlocale(LC_ALL,"Russian");
-    cout<<endl<<"######################## ZIPPER ########################"<<endl<<endl;
-    if(argv>1)
+    setlocale(LC_ALL, "Russian");
+    cout << endl
+         << "######################## ZIPPER ########################" << endl
+         << endl;
+    if (argv > 1)
     {
-        vector<string> files;  // массив файлов, переданных через параметры из консоли
-        string path = "";  // путь
-        bool flag_fs = false, flag_path = false;  // флаги режима чтения/записи
-        char type[6];              // тип: упаковка или распаковка 
-        memset(type,0,6);
+        vector<string> files;                    // массив файлов, переданных через параметры из консоли
+        string path = "";                        // путь
+        bool flag_fs = false, flag_path = false; // флаги режима чтения/записи
+        char type[7];                            // тип: упаковка или распаковка
+        memset(type, 0, 7);
 
-        for(int i=1;i<argv;i++)
+        for (int i = 1; i < argv; i++)
         {
-            if(strcmp(argc[i],"-pack")==0) { strcpy(type,"pack"); flag_fs=flag_path=false;}
-            if(strcmp(argc[i],"-unpack")==0) { strcpy(type,"unpack"); flag_fs=flag_path=false;}
-            if(strcmp(argc[i],"-path")==0) {flag_path=true; flag_fs=false; continue; }
-            if(strcmp(argc[i],"-files")==0) {flag_fs=true; flag_path=false; continue; }
+            if (strcmp(argc[i], "-pack") == 0)
+            {
+                strcpy(type, "pack");
+                flag_fs = flag_path = false;
+            }
+            if (strcmp(argc[i], "-unpack") == 0)
+            {
+                strcpy(type, "unpack");
+                flag_fs = flag_path = false;
+            }
+            if (strcmp(argc[i], "-path") == 0)
+            {
+                flag_path = true;
+                flag_fs = false;
+                continue;
+            }
+            if (strcmp(argc[i], "-files") == 0)
+            {
+                flag_fs = true;
+                flag_path = false;
+                continue;
+            }
 
-            if(flag_path) {path.assign(argc[i]); }
-            if(flag_fs) files.push_back(string(argc[i]));
-
+            if (flag_path)
+            {
+                path.assign(argc[i]);
+            }
+            if (flag_fs)
+                files.push_back(string(argc[i]));
         }
-        Zipper *zip = new Zipper(files,path);
-        if(strcmp(type,"pack")==0) zip->InCompress();
-        if(strcmp(type,"unpack")==0) zip->OutCompress(files[0]);
+        Zipper *zip = new Zipper(files, path);
+        if (strcmp(type, "pack") == 0)
+            zip->InCompress();
+        if (strcmp(type, "unpack") == 0)
+            zip->OutCompress(files[0]);
     }
-    else cout<<"Параметры -pack/-unpack , -files, -path обязательны!"<<endl;
-    cout<<endl<<"########################################################"<<endl<<endl;
-
+    else
+        cout << "Параметры -pack/-unpack , -files, -path обязательны!" << endl;
+    cout << endl
+         << "########################################################" << endl
+         << endl;
 }
